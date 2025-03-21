1527c93bd7482c45fe7c42bc76da4a13541ffa0e
SPARSH RAJPUT <rajputsparsh10sr@gmail.com>
2025-03-21 14:07:58 +0530

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,27 @@
+// layout
+export { default as BaseLayout } from "./layout/BaseLayout";
+// header
+export { default as Header } from "./header/Header";
+// footer
+export { default as Footer } from "./footer/Footer";
+// home
+export { default as Banner } from "./home/Banner/Banner";
+// common
+export { default as Genre } from "./common/genre/Genre/Genre";
+export { default as GenreItem } from "./common/genre/GenreItem/GenreItem";
+export { default as SectionTitle } from "./common/SectionTitle/SectionTitle";
+export { default as FreeTrial } from "./common/FreeTrial/FreeTrial";
+export { default as Spinner } from "./common/Spinner/Spinner";
+export { default as ErrorMessage } from "./common/ErrorMessage/ErrorMessage";
+export { default as ShowsList } from "./common/shows/ShowsList/ShowsList";
+export  { default as ShowsItem } from "./common/shows/ShowsItem/ShowsItem";
+export { default as ShowsBanner } from "./common/shows/ShowsBanner/ShowsBanner";
+export { default as ShowsSlider } from "./common/shows/ShowsSlider/ShowsSlider";
+export { default as NoDataFound } from "./common/NotDataFound/NotDataFound";
+// search
+export { default as SearchBar } from "./search/SearchBar/SearchBar";
+export { default as SearchList } from "./search/SearchList/SearchList";
+//shows
+export { default as ShowMainData } from "./shows/ShowMainData/ShowMainData";
+export { default as ShowMetaData } from "./shows/ShowMetaData/ShowMetaData";
+ 
